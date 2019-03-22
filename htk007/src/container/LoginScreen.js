f03746c3b59318a7fb8dc9dd176be89d8d366a42
dongnh <dongnh@topica.edu.vn>
2019-03-22 22:23:47 +0700

    add ui screen login

--- htk007/src/container/LoginScreen.js
+++ htk007/src/container/LoginScreen.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, TextInput, Image, ImageBackground } from 'react-native';
 import { CheckBox } from 'react-native-elements'
-import { Actions } from 'react-native-router-flux'
+import { Actions } from 'react-native-router-flux';
+import LinearGradient from "react-native-linear-gradient";
+
 
 class LoginScreen extends Component {
     constructor(props) {
@@ -19,7 +21,7 @@ class LoginScreen extends Component {
 
     render() {
         return (
-            <ImageBackground source={require('../../assets/images/backgroundlogin.png')} style={styles.container}>
+            <ImageBackground source={require('../../assets/images/bg_login.png')} style={styles.container}>
                 {this.renderMainContent()}
             </ImageBackground>
         );
@@ -28,27 +30,56 @@ class LoginScreen extends Component {
     renderMainContent = () => {
         return (
             <View style={{ alignItems: 'center', justifyContent: 'center', width: '100%' }}>
-                <Text style={{ marginVertical: 15, fontSize: 24, color: 'white', fontWeight: 'bold' }}>{'Đăng nhập'} </Text>
+                <Image source={require('../../assets/images/logo.png')} style={{ marginVertical: 15}}/>
                 <TextInput
-                    style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '100%', marginVertical: 15 }}
+                    style={{ height: 40,borderRadius:5, width: '100%', marginVertical: 15,backgroundColor: 'white', }}
                     onChangeText={(text) => this.setState({ userame: text })}
                     value={this.state.text}
+                    placeholder={'  Tên đăng nhập'}
+                    placeholderTextColor={'#c6c6c6'}
+                    
                 />
 
                 <TextInput
-                    style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '100%', }}
+                    style={{ height: 40, borderRadius:5, width: '100%',backgroundColor: 'white', }}
                     onChangeText={(text) => this.setState({ password: text })}
                     value={this.state.text}
+                    placeholder={'  Mật khẩu'} 
+                    placeholderTextColor={'#c6c6c6'}
                 />
+                <View style={{ flexDirection: 'row', alignItems: 'center',width: '100%', }}>
+                    <CheckBox
+                        textStyle = {{fontSize: 16, color: '#707070'}}
+                        title='Nhớ mật khẩu'
+                        checked={this.state.checked}
+                        containerStyle={{ backgroundColor: 'transparent',borderColor:'transparent' }}
+                    />
 
-                <CheckBox
-                    title='Ghi nhớ mật khẩu'
-                    checked={this.state.checked}
-                />
+                    <Text style={{ fontSize: 16, color: '#707070', }}>{'Quên mật khẩu?'}</Text>
+                </View>
 
-                <TouchableOpacity onPress={() => this.onClickLogin()} style={{ justifyContent: 'center', alignItems: 'center', width: '100%', height: 50 }}>
-                    <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 14 }} >{'ĐĂNG NHẬP'}</Text>
+                <LinearGradient
+                    style={styles.loginbutton}
+                    start={{ x: 1, y: 0 }}
+                    end={{ x: 0, y: 1 }}
+                    colors={['#b92b27', "#1565C0"]}
+                >
+                    <TouchableOpacity onPress={() => this.onClickLogin()} style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 19 }} >{'ĐĂNG NHẬP'}</Text>
+                    </TouchableOpacity>
+                </LinearGradient>
+
+
+                <View style={[styles.loginbutton, { backgroundColor: 'transparent' }]}>
+                    <Text style={{ fontSize: 16, color: '#707070' }}>{'Hoặc'} </Text>
+                </View>
+                <TouchableOpacity style={[styles.loginbutton, { flexDirection: 'row', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }]} >
+                    <Image source={require('../../assets/images/google_icon.png')} style={{ width: 25, height: 25, marginRight: 10 }}  ></Image>
+                    <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#383838' }}> {'Đăng nhập với Google'}</Text>
                 </TouchableOpacity>
+
+
+
             </View>
         )
     }
@@ -59,7 +90,15 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        paddingHorizontal: 15,
+        paddingHorizontal: 25,
+    },
+    loginbutton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: 50,
+        marginTop: 10,
+        borderRadius: 5
     }
 });
 
